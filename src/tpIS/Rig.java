97eb10faa341c719b59baadb61b5a5c8a93b7585
Alex3D <Alex3D@AleNote>
2017-05-28 19:31:33 -0300

    Se agrega el rig

--- src/tpIS/Rig.java
+++ src/tpIS/Rig.java
@@ -1,5 +1,30 @@
 package tpIS;
 
 public class Rig {
-
+	private int velocidadDeCavadoXDia = 1;
+	private double consumCombustibleXDia = 1;
+	private int metrosCavados = 0;
+	private boolean cavando = false;
+	
+	public Rig(int VelocidadDeCavadoXDIA, int ConsumoDeCombustibleXDIA) {
+		velocidadDeCavadoXDia = VelocidadDeCavadoXDIA;
+		consumCombustibleXDia = ConsumoDeCombustibleXDIA;
+	}
+	
+	public void cavarPozoEnParcela(Parcela unaParcela, EstadoFinanciero unEstadoFinanciero) {
+		if(!cavando) {
+			cavando = true;
+			if(metrosCavados < unaParcela.getProfundidad()) {
+				metrosCavados += velocidadDeCavadoXDia/ unaParcela.getTipoDeTerreno().tipoDeTerreno();
+				unEstadoFinanciero.debit(consumCombustibleXDia);
+			}
+			else {
+				unaParcela.crearPoso();
+				metrosCavados =0;
+				cavando = false;
+			}
+		}
+		else 
+			System.err.println("Un Rig no puede cavar en mas de un pozo a la ves");
+	}
 }
