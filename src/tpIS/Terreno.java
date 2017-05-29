ebaa3949d6a3eba20a5dfade180abed3ad1cc4c5
Alex3D <Alex3D@AleNote>
2017-05-29 03:11:08 -0300

    Cambios en Terreno, para q responda a la resistencia a ser cavado

--- src/tpIS/Terreno.java
+++ src/tpIS/Terreno.java
@@ -2,4 +2,6 @@ package tpIS;
 
 public interface Terreno {
 	public int tipoDeTerreno();
+	
+	public int resistenciaAlRig();
 }
--- src/tpIS/TerrenoRocoso.java
+++ src/tpIS/TerrenoRocoso.java
@@ -1,8 +1,18 @@
 package tpIS;
 
 public class TerrenoRocoso implements Terreno{
+	private int resistencia = 0;
+	
+	public TerrenoRocoso(int resistenciaDelTerreno) {
+		resistencia = resistenciaDelTerreno;
+	}
+	
 	public int tipoDeTerreno(){
         System.out.println("Rocoso");
         return 1;
+	}
+	
+	public int resistenciaAlRig() {
+		return resistencia;
 	}
 }
