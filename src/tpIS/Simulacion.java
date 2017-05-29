5f02ef7ea1e673269d4eae2865719d0a0017faad
Alex3D <Alex3D@AleNote>
2017-05-29 05:21:51 -0300

    avanzando con simulacion

--- src/tpIS/Simulacion.java
+++ src/tpIS/Simulacion.java
@@ -1,4 +1,5 @@
 package tpIS;
+import java.util.ArrayList;
 import java.util.List;
 import tpIS.Closure;
 
@@ -8,12 +9,19 @@ public class Simulacion {
 	private Context _context;
 	private Logger _logger;
 	private Reader _reader;
+	private List<Rig> rigsCavando;
+	private List<Tanque> tanquesEnConstruccion;
+	private List<Planta> plantasEnConstruccion;
 
 	public Simulacion(){
 		this._reader = new Reader();
 		this._equipoIngenieria=  this._reader.getEquipoIngenieria();// new EquipoIngenieria(this._reader);
 		this._context= new Context(this._reader);
 		this._logger= new Logger();
+		
+		rigsCavando = new ArrayList<Rig>();
+		tanquesEnConstruccion = new ArrayList<Tanque>();
+		plantasEnConstruccion = new ArrayList<Planta>();
 	}
 
 
@@ -26,6 +34,7 @@ public class Simulacion {
 				action.Apply(this._context, this._equipoIngenieria);
 				this._logger.Log(this._context.GetLastEventsToLog());				
 			}
+			tareasEnSimulacion();
 			/*this._logger.Log("Estado financiero actual: "+
 					String.valueOf(this._equipoIngenieria.getEstadoFinanciero().getStatus()));*/
 			this._logger.Log("Finaliza el dia "+ String.valueOf(this._context.GetDay()));
@@ -35,4 +44,32 @@ public class Simulacion {
 
 	}
 	
+	private void tareasEnSimulacion() {
+		//Seguimos contrullendo los pozos
+		if(!rigsCavando.isEmpty()) {
+			for(Rig unRig : rigsCavando){
+				if(unRig.isCavando())
+					unRig.seguirCavando();
+				else
+					rigsCavando.remove(unRig);
+			}			
+		}
+		//seguimos construllendo los tanques
+		if(!tanquesEnConstruccion.isEmpty()){
+			for(Tanque unTanque : tanquesEnConstruccion){
+				if(!unTanque.estaCosntruido())
+					unTanque.construirUnDia();
+				else
+					tanquesEnConstruccion.remove(unTanque);
+			}
+		}
+		if(!plantasEnConstruccion.isEmpty()){
+			for(Planta unaPlanta : plantasEnConstruccion){
+				if(!unaPlanta.plantaEnConstruccion())
+					unaPlanta.construirUnDia();
+				else
+					plantasEnConstruccion.remove(unaPlanta);
+			}
+		}
+	}
 }
