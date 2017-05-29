a2acf0e31a7a72c3ef39a6e07cd5c2dd41f66a06
Alex3D <Alex3D@AleNote>
2017-05-29 00:22:21 -0300

    Implementacion de tanque

--- src/tpIS/Tanque.java
+++ src/tpIS/Tanque.java
@@ -1,5 +1,35 @@
 package tpIS;
 
 public abstract class Tanque {
-
+	protected int diasDeConstruccionActual;
+	protected int diasDeConstruccionTotal;
+	//protected double costoConstruccion;
+	protected double volumenTotal;
+	protected double volumenOcupadoActual;
+	
+	public Tanque(double volumen, int diasDeConstruccion) {
+		diasDeConstruccionTotal= diasDeConstruccion;
+		volumenTotal = volumen;
+		volumenOcupadoActual =0;
+		diasDeConstruccionActual =0;
+	}
+	
+	public double getVolumenTotal() {
+		return volumenTotal;
+	}
+	public double getVolumenOcupadoActual() {
+		return volumenOcupadoActual;
+	}
+	
+	protected void extraerVol(double vol) {
+		this.volumenOcupadoActual -= vol;
+	}
+	
+	public void llenar(double vol) {
+		volumenOcupadoActual += vol;
+	}
+	
+	public boolean estaCosntruido() {
+		return diasDeConstruccionActual == diasDeConstruccionTotal;
+	}
 }
