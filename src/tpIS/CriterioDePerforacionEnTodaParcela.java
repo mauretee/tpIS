8a3395a55c6f8ee531486e007e1453f4bf4ab95a
Alex3D <Alex3D@AleNote>
2017-06-07 21:51:15 -0300

    programacion orientada a casteos

--- src/tpIS/CriterioDePerforacionEnTodaParcela.java
+++ src/tpIS/CriterioDePerforacionEnTodaParcela.java
@@ -15,7 +15,7 @@ public class CriterioDePerforacionEnTodaParcela extends CriterioDePerforacion{
 			Function perforar = new Function() {
 		        @Override
 		        public void Apply(Context context, Equipo equipo, Object anObject)  {
-		        	int i = 0;
+		        	//int i = 0;
 		        	for(Parcela parcela : equipo._yacimiento.listaDeParcelas){		        		
 		        		if(!parcela.tienePozo() && !parcela.pozoEnConstruccion()) {
 		        			
@@ -25,15 +25,16 @@ public class CriterioDePerforacionEnTodaParcela extends CriterioDePerforacion{
 		        			 */
 		        			CriterioEleccionDeRigs unCriterioRig = new CriterioEleccionDeRigsxMasBarato(equipo);
 		        			Rig Elejido= null;
-		        			unCriterioRig.Evaluate(context).get(0).Apply(context, equipo, (Object)Elejido);
+		        			unCriterioRig.Evaluate(context).get(0).Apply(context, equipo, Elejido);
+		        			Elejido = (Rig) Elejido;
 		        			Elejido.cavarPozoEnParcela(parcela);
 		        			
-		        			Rig rig = context.getRigs().get(i);
-			        		rig.cavarPozoEnParcela(parcela);
+		        			/*Rig rig = context.getRigs().get(i);
+			        		rig.cavarPozoEnParcela(parcela);*/
 			        		//TODO: llamar al calculador de costo y 
 		        		}
 		        				        		
-		        		i++;
+		        		//i++;
 		        		
 		        	}
 		        }
