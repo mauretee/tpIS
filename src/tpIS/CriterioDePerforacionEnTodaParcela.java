5a2dfbfec0ab691424af7bb387334c922ba477b9
Alex3D <Alex3D@AleNote>
2017-07-03 23:17:01 -0300

    Voy a necesitar ayuda con el closer, y si pueden ir berificando que no haya fruta en los commits q mande hoy, me ayudarian mucho

--- src/tpIS/CriterioDePerforacionEnTodaParcela.java
+++ src/tpIS/CriterioDePerforacionEnTodaParcela.java
@@ -15,7 +15,6 @@ public class CriterioDePerforacionEnTodaParcela extends CriterioDePerforacion{
 			Function perforar = new Function() {
 		        @Override
 		        public void Apply(Context context, Equipo equipo)  {
-		        	//int i = 0;
 		        	for(Parcela parcela : equipo._yacimiento.listaDeParcelas){		        		
 		        		if(!parcela.tienePozo() && !parcela.pozoEnConstruccion()) {
 		        			
